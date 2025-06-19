e7d95bfa3ce3f9e27c0a31e4d956e12751160038
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2025-06-19 16:23:50 +0200

    update db sql

--- db.sql
+++ db.sql
@@ -1,12 +1,12 @@
--- CREATE TABLE customers (
---     id SERIAL PRIMARY KEY,
---     first_name varchar(50),
---     last_name varchar(50),
---     email varchar(100) UNIQUE,
---     address varchar(100),
---     username varchar(50) UNIQUE,
---     password varchar(50)
--- );
+CREATE TABLE customers (
+    id SERIAL PRIMARY KEY,
+    first_name varchar(50),
+    last_name varchar(50),
+    email varchar(100) UNIQUE,
+    address varchar(100),
+    username varchar(50) UNIQUE,
+    password varchar(50)
+);
 
 CREATE TABLE products (
     id SERIAL PRIMARY KEY,
@@ -15,28 +15,34 @@ CREATE TABLE products (
     price_cents INTEGER
 );
 
--- CREATE TABLE orders (
---     id SERIAL PRIMARY KEY,
---     customer_id INTEGER REFERENCES customers(id),
---     placed_at datetime,
---     status varchar(100)
--- );
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    customer_id INTEGER REFERENCES customers(id),
+    placed_at datetime,
+    status varchar(100)
+);
 
--- CREATE TABLE orders_products (
---     product_id INTEGER REFERENCES products(id),
---     order_id INTEGER REFERENCES orders(id),
---     quantity INTEGER,
---     price_cents INTEGER
--- );
+CREATE TABLE orders_products (
+    product_id INTEGER REFERENCES products(id),
+    order_id INTEGER REFERENCES orders(id),
+    quantity INTEGER,
+    price_cents INTEGER
+);
 
--- CREATE TABLE carts (
---     id SERIAL PRIMARY KEY,
---     customer_id INTEGER REFERENCES customers(id),
---     ttl datetime
--- );
+CREATE TABLE carts (
+    id SERIAL PRIMARY KEY,
+    customer_id INTEGER REFERENCES customers(id),
+    ttl datetime
+);
 
--- Importing a products csv
+-- Importing a products csv (via psql)
 COPY products (name, brand, price_cents)
 FROM 'C:/Users/Elena/Desktop/toy-store/db_products.csv'
+DELIMITER ','
+CSV HEADER;
+
+-- Importing a customers csv (vie psql)
+COPY products (name, brand, price_cents)
+FROM 'C:/Users/Elena/Desktop/toy-store/db_customers.csv'
 DELIMITER ','
 CSV HEADER;
