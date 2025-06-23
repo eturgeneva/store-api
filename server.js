05465e6642a9e88503286c11e48a64444dfbcf0c
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2025-06-23 18:39:33 +0200

    update deserializeUser

--- server.js
+++ server.js
@@ -87,13 +87,14 @@ passport.serializeUser((user, done) => {
     done(null, user.id);
 });
 
-// passport.deserializeUser((user, done) => {
-//     done(null, user);
-// });
-passport.deserializeUser((id, done) => {
-  db.get('SELECT * FROM users WHERE id = ?', [id], function(err, user) {
-    done(err, user);
-  });
+passport.deserializeUser(async (id, done) => {
+  try {
+    const result = await pool.query('SELECT id, username FROM customers WHERE id = $1', [id]);
+    if (result.rows.length === 0) return done(null, false);
+    return done(null, result.rows[0]);
+  } catch (err) {
+    return done(err);
+  }
 });
 
 app.get('/', (req, res) => {
