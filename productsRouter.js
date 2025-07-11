a36b744d81fc6b35f970697ae10b7ff52d9c11ff
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2025-07-11 11:51:31 +0200

    add responses and error handling for product routes

--- productsRouter.js
+++ productsRouter.js
@@ -12,15 +12,29 @@ const pool = new Pool({
 // Get all products:
 productsRouter.get('/', async (req, res, next) => {
     // res.send({ products: 'Product 1'});
-
-    const products = await pool.query('SELECT * FROM products');
-    console.log(products);
-    // res.status(200).send({ products: products.rows });
-    res.status(200).send(products.rows);
+    try {
+        const products = await pool.query('SELECT * FROM products');
+        console.log(products);
+        // res.status(200).send({ products: products.rows });
+        res.status(200).send(products.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 productsRouter.get('/:id', async (req, res, next) => {
-    
+    try {
+        const product = await pool.query('SELECT * FROM products WHERE id = $1', [req.params.id]);
+        if (product.rows.length === 0) {
+            res.status(404).send('Product not found')
+        }
+        // res.status(200).send(product);
+        res.status(200).send(product.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+    }
 })
 
 module.exports = productsRouter;
