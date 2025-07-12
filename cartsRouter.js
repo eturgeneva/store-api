3f89cac3620773f927210827a5aa23bc697ad681
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2025-07-12 11:33:52 +0200

    wip cart creation route

--- cartsRouter.js
+++ cartsRouter.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const { format, addMonths } = require('date-fns');
 const cartsRouter = express.Router();
 const { pool } = require('./pool');
 
+const expirationTimeMonth = 3;
+
 // Create a new cart:
 cartsRouter.post('/', async (req, res, next) => {
+
+    // const cartExpirationDate = Math.floor(Date.now()/1000);
+    const cartExpirationDate = addMonths(new Date(), expirationTimeMonth);
+    console.log('cart expiration date', cartExpirationDate);
     try {
         const newCart = await pool.query(
-            'INSERT INTO carts (customer_id) VALUES ($1) RETURNING id', 
-            // [req.user.id]
-            [null]
+            'INSERT INTO carts (customer_id, expires_at) VALUES ($1, $2) RETURNING id', 
+            [null, format(cartExpirationDate, 'yyyy-MM-dd HH:mm:ss')]
         )
         if (newCart.rows.length === 1) {
             res.status(201).send({ cartId: newCart.rows[0].id });
