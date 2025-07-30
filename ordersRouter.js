df69d1a747923abaa0b6db66fbbc125d34b5eb01
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2025-07-30 11:52:22 +0200

    wip order data processing and storing inside ordersRouter

--- ordersRouter.js
+++ ordersRouter.js
@@ -4,13 +4,36 @@ const { pool } = require('./pool');
 
 // Place a new order
 ordersRouter.post('/', async (req, res, next) => {
+    const { products } = req.body;
+    console.log('Products from request body', products);
+    
     try {
         const newOrder = await pool.query(
             'INSERT INTO orders (customer_id, status) VALUES ($1, $2) RETURNING *',
             [req.user?.id ?? null, 'received']
         )
         if (newOrder.rows.length === 1) {
-            res.status(201).send({ order: newOrder.rows[0] });
+            // res.status(201).send({ order: newOrder.rows[0] });
+
+            // Adding order info into the database:
+            products.forEach(async (product) => {
+                const newOrderDetails = await pool.query(
+                    'INSERT INTO orders_products (product_id, order_id, quantity, price_cents) VALUES ($1, $2, $3, $4) RETURNING *',
+                    [product.product_id, newOrder.rows[0].id, product.quantity, product.price_cents]
+                );
+                if (newOrderDetails.rows.length !== 1) {
+                    return res.status(400).send('Failed to store order data');
+                }
+            });
+    
+            // Combined result from orders and orders_products
+            const orderDetails = await pool.query(
+                'SELECT * FROM orders JOIN orders_products ON orders.id = orders_products.order_id WHERE orders.id = $1',
+                [newOrder.rows[0].id]
+            );
+
+            res.status(201).send({ order: orderDetails.rows });
+
         } else {
             res.status(400).send('Failed to place an order')
         }
@@ -19,6 +42,7 @@ ordersRouter.post('/', async (req, res, next) => {
         console.error(err);
         res.status(500).send('Internal Server Error');
     }
+
 
 });
 
