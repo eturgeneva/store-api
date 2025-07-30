d5cacd1b74225a4b809b7a67a99a642e39644b54
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2025-07-30 10:39:27 +0200

    wip orders post route

--- ordersRouter.js
+++ ordersRouter.js
@@ -5,7 +5,19 @@ const { pool } = require('./pool');
 // Place a new order
 ordersRouter.post('/', async (req, resizeBy, next) => {
     try {
-        
+        const newOrder = await pool.query(
+            'INSERT INTO orders (customer_id, status) VALUES ($1, $2) RETURNING *',
+            [req.user?.id ?? null, 'received']
+        )
+        if (newOrder.rows.length === 1) {
+            res.status(201).send({ order: newOrder.rows[0] });
+        } else {
+            res.status(400).send('Failed to place an order')
+        }
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
     }
 
 });
