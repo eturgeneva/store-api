4f0f6f18da8df3602892a81a3347c841d48a51d9
Elena Turgeneva <elena.turgeneva@gmail.com>
2026-06-02 15:36:14 +0200

    update server.js

--- server.js
+++ server.js
@@ -4,5 +4,3 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`App running on http://localhost:${PORT}`);
 });
-
-// /c/Users/Elena/ngrok http --domain aardvark-cool-weasel.ngrok-free.app 3000
