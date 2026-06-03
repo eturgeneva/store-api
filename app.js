3f70d39da817ceee73f44db120bbe5b1fd580327
Elena Turgeneva <elena.turgeneva@gmail.com>
2026-06-03 22:48:01 +0200

    allow overriding base domain for deployment

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ passport.use(new LocalStrategy(
 passport.use(new GoogleStrategy({
     clientID: process.env['GOOGLE_CLIENT_ID'],
     clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
-    callbackURL: 'http://localhost:3000/oauth2/redirect/google',
+    callbackURL: `${process.env.BASE_URL}/oauth2/redirect/google`,
     scope: [ 'profile', 'email' ],
     state: true
   },
